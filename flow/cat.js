07b23ddc7a35e13039245b8de28863d135b46f57
zhangmin <zhangmin@guahao.com>
2018-06-11 17:49:03 +0800

    提交

---
+++ flow/cat.js
@@ -0,0 +1,10 @@
+
+export type cat={
+  name:string;
+  speak:Function
+}
+
+declare type CompiledResult = {
+
+  tips: Array<string>;
+};
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script type="text/javascript">
+/* flow */
+import type {
+  cat
+} from '../flow/cat'
+
+export default {
+  name: 'App',
+  created () {
+    this.sum({
+      name: '1',
+      speak () {}
+    }, 1)
+  },
+  methods: {
+    sum (a:cat, b:number):number {
+      console.info(a)
+      console.info(a)
+      console.info(a)
+      console.info(a)
+      return b
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
